61f1d508b630d402c8e076f3902113353e898048
BawkYin <bawk_yin@126.com>
2026-08-29 19:05:19 +0800

    完善了单方块机器类型，还有些没写，但是应该不会影响产线规划

--- src/main.rs
+++ src/main.rs
@@ -42,3 +42,71 @@ pub struct OverClockRecipe {
     pub outputs: HashMap<String, (u64, f64)>,
     pub catalysts: HashMap<String, u64>,
 }
+
+/// 这是单方块机器
+pub enum SingleKind {
+    /// 处理类机器
+    Assembler, // 组装机
+    CircuitAssembler, // 电路组装机
+    Mixer,            //搅拌机
+    LaserEngraver,    // 激光蚀刻机
+    CuttingMachine,   // 切割机
+    Compressor,       // 压缩机
+    Autoclave,        // 高压釜
+    /// 锻造类机器
+    BendingMachine, // 卷板机
+    FormingPress,     // 冲压机床
+    ForgeHammer,      // 锻造锤
+    Extruder,         // 压膜机
+    Lathe,            // 车床
+    Wiremill,         // 线材轧机
+    /// 分离类机器
+    Extractor, // 提取机
+    FluidExtractor,   // 流体提取机
+    Centrifuge,       // 离心机
+    Electrolyzer,     // 电解机
+    ElectromagneticSeparator, // 电磁离析机
+    ElectromagneticPolarizer, // 两极磁化机
+    Dehydrator,       // 化学脱水机
+    /// 化学类机器
+    ChemicalReactor, // 化学反应釜
+    Distillery,       // 蒸馏室
+    Brewery,          // 酿造室
+    Fermenter,        // 发酵槽
+    BioLab,           // 生物实验室
+    ColdTrap,         // 冷阱
+    ReactorProcessingUnit, // 反应堆处理单元
+    /// 加热类机器
+    ElectricFurnace, // 电炉
+    ElectricOven,     // 电烤炉
+    Microwave,        // 微波炉
+    AlloySmelter,     // 合金炉
+    FluidHeater,      // 流体加热器
+    ArcFurnace,       // 电弧炉
+    /// 矿石处理类机器
+    SeismicProspector, // 地震勘探者
+    Pump,             // 泵
+    Miner,            // 采矿机
+    Macerator,        // 研磨机
+    SimpleWasher,     // 简易洗矿池
+    OreWasher,        // 洗矿厂
+    ChemicalBath,     // 化学浸洗机
+    Sifter,           // 筛选机
+    ThermalCentrifuge, // 热力离心机
+    /// 打包类机器
+    Canner, // 装罐机
+    Packager,         // 打包机
+    Unpackager,       // 解包器
+    /// 其他机器
+    RockBreaker, // 碎石机
+    AutoChisel,       // 自动雕凿机
+}
+
+/// 这是多方块机器
+/// 用到的时候在写
+pub enum MultiKind {}
+
+pub enum AnyMachine {
+    SingleBlock(SingleKind),
+    MultiBlock(MultiKind),
+}
